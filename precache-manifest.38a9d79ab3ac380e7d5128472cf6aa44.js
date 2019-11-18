bf535ee36190c7145bafc54832248e65c2f64fe4
rizkyari <rizkyari4121@gmail.com>
2019-11-18 16:45:40 +0700

    Updates

---
+++ precache-manifest.38a9d79ab3ac380e7d5128472cf6aa44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ff25e8228f230d97a9aa052bea6b68",
+    "url": "/personnel-list/index.html"
+  },
+  {
+    "revision": "a6ab42e1c5aea33f4cb8",
+    "url": "/personnel-list/static/css/main.5e8d2f30.chunk.css"
+  },
+  {
+    "revision": "13f4f1012f937803a63e",
+    "url": "/personnel-list/static/js/2.b72eae19.chunk.js"
+  },
+  {
+    "revision": "a6ab42e1c5aea33f4cb8",
+    "url": "/personnel-list/static/js/main.b0b84e40.chunk.js"
+  },
+  {
+    "revision": "c3d37b0766ca63205b80",
+    "url": "/personnel-list/static/js/runtime-main.3ecb0367.js"
+  }
+]);
